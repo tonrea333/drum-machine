4285a85db36c45a50ffa0095cb0f06d2288e9532
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-07-26 12:36:05 -0400

    all

--- main.js
+++ main.js
@@ -6,12 +6,31 @@ function update() {
 
     // Play the 'tick' sound
     tick.play();
+
 }
 
 // This function sets up update() to be called every 600ms
 function setupUpdate() {
-    setInterval(update, 600);
+    setInterval(update, 600)
+    //setTimeout(450);
 }
 
 // Call setupUpdate() once after 300ms
 setTimeout(setupUpdate, 300);
+
+//Setup 'tock' sound
+const tock = new Audio("sounds/tock.mp3")
+
+//This function is called every 2400ms
+function update2(){
+    //Play the "tock" sound
+   tock.play()
+}
+
+//This function sets up update() to be calle every 2400ms
+function setuptockUpdate() {
+    setInterval(update2, 2400);
+}
+
+//call setuptockupdate() once after 1200ms
+setTimeout(setuptockUpdate, 1200)
