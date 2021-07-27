6342be4159c9cc3912fa2035b71679fe029d887f
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-07-27 12:51:11 -0400

    all

--- main.js
+++ main.js
@@ -7,7 +7,12 @@
 const tick = new Audio('sounds/tick.mp3');
 //Setup 'tock' sound
 const tock = new Audio("sounds/tock.mp3")
-
+//Setup "kick-drum" sound
+const kickDrum = new Audio("sounds/kick-drum.mp3");
+//Setup "snare-drum" sound
+const snareDrum = new Audio("sounds/snare-drum.mp3");
+//Setup "hi-hat" sound
+const hiDrum = new Audio("sounds/hi-hat.mp3");
 
 //Metronome event listner
 
@@ -20,32 +25,56 @@ let counter = 0;
 
 // This function is called every 600ms
 function update() {
-    const metroSound = document.querySelectorAll("input[type=checkbox]")
-
+    const metroSound = document.querySelectorAll('input[name="beat"]:checked');
+    const kickSound = document.querySelectorAll('input[name="aaaa"]:checked');
+    const snareSound = document.querySelectorAll('input[name="cccc"]:checked');
+    const hiSound = document.querySelectorAll('input[name="dddd"]:checked');
     for (let i = 0; i < metroSound.length; i++) {
         const metroChk = metroSound[i];
-        if (metroChk.checked) {
-
-
+        if (metroChk) {
             console.log(metroChk)
-
-
-
             counter = counter + 1;
             // Play the 'tick' sound
             tick.play();
-
             //Play the "tock" sound
             if (counter % 4) {
                 tock.play()
 
+                console.log(metroSound[i], "test")
                 console.log(counter)
 
                 const metroncount = document.querySelector(".BeatCounter");
                 metroncount.innerText = counter;
             }
         }
+
     }
+    //loop for kick sound
+    for (let i = 0; i < kickSound.length; i++) {
+        const kickChk = kickSound[i];
+        if (kickChk.checked) {
+            kickDrum.play();
+        }
+    }
+//loop for snare sound
+    for (let i = 0; i < snareSound.length; i++) {
+        const snareChk = snareSound[i];
+        if (snareChk.checked) {
+            snareDrum.play();
+        }
+    }
+    //Loop for hi sound
+    for (let i = 0; i < hiSound.length; i++) {
+        const hiChk = hiSound[i];
+        if (hiChk.checked) {
+            hiDrum.play();
+        }
+    }
+
+
+
+
+
 }
 // This function sets up update() to be called every 600ms
 function setupUpdate() {
